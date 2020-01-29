8243017f63e2f37db8667992f0ae6f3db8084a23
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2020-01-29 20:54:22 +0100

    update docker and kube script

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=rhotimee/ml-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run ml-api --image=rhotimee/ml-api --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
+kubectl expose deployment ml-api --type=LoadBalancer --port=80
 
+# Open the service 
+minikube service ml-api
